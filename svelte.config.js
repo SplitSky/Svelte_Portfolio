cfbe22c9953a37271c1e6d738e7afbc93a6f4579
SplitSky <24545133+SpitSky@users.noreply.github.com>
2026-08-17 20:04:52 +0100

    cicd: added github actions for compiling and added changes to config file

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			// base: dev ? '' : process.env.BASE_PATH || ''
+			base: process.env.NODE_ENV === 'production' ? '/Svelte_portfolio' : ''
+		}
 	},
 	preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
 	extensions: ['.svelte', '.svx', '.md']
