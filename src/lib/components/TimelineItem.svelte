8eb9905bad1fd51fd6c602b42c5bb94b2b7a5f9d
SplitSky <24545133+SplitSky@users.noreply.github.com>
2026-08-27 20:31:25 +0100

    feat: Tried making a timeline item. type issues in declaration

---
+++ src/lib/components/TimelineItem.svelte
@@ -0,0 +1,27 @@
+<script>
+	import TimelineItem from "$lib/components/TimelineItem.svelte";
+	import { describe } from "node:test";
+
+	let steps = [
+		{
+			title: 'First Project',
+			description: 'First description',
+			state: 'complete'
+		},
+		{
+			title: 'Second Project',
+			description: 'Second description',
+			state: 'pending'
+		},
+		{
+			title: 'Third Project',
+			description: 'Third description',
+			state: 'current'
+		}
+
+	]
+</script>
+
+{#each steps as step}
+	<TimelineItem {...step} />
+{/each}
