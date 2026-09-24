bc35c4b663f58d4163cc833451c4265620aa1468
SplitSky <24545133+SpitSky@users.noreply.github.com>
2026-09-24 19:53:12 +0100

    fix: Fixed the svelte config for static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,6 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 	},
 	kit: {
@@ -14,10 +13,7 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.env.BASE_PATH || ''
-		}
+		})
 	},
 	preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
 	extensions: ['.svelte', '.svx', '.md']
